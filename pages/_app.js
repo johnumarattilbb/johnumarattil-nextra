fa67129b3d8cd310974e467938dc7fa42f7e89db
johnumarattil <johnumarattil@gmail.com>
2024-03-13 11:52:57 -0400

    Ganalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,47 @@
 import './styles.css'
- 
+import { pageview } from '../lib/gtag';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${pageview.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${pageview.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
