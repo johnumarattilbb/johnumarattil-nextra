99f2dfb3b3cc66a4c677692d28bf0ecffa74993b
johnumarattil <johnumarattil@gmail.com>
2024-03-13 21:32:00 -0400

    projects routing to coins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/projects',
+        destination: '/projects/coins', // Assuming you want to redirect to this path
+        permanent: true, // If this is a permanent redirect, set to true. Change to false if temporary.
+      },
+    ];
+  },
+});
