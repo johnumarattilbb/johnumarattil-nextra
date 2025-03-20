1899af7128e58b232dfac5483501ccc9a2ec79ff
johnumarattilbb <johnumarattil@gmail.com>
2025-03-20 13:47:06 -0400

    Update theme configuration with a more detailed description and a personalized title for the portfolio

--- theme.config.jsx
+++ theme.config.jsx
@@ -23,8 +23,8 @@ export default {
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-            <meta name="description" content="Your project description" />
-            <meta name="og:title" content="Your Project Name" />
+            <meta name="description" content="Johnu Marattil is a UI/UX Engineer from Toronto with 7 years of experience in designing and developing intuitive solutions for enterprise-scale web and mobile applications" />
+            <meta name="og:title" content="Portfolio - Johnu Marattil" />
         </>
     ),
     search: {
