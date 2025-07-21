f75a1dd781f741bcf8d1d04f9f633aca2f9b0bda
johnumarattilbb <89916822+johnumarattilbb@users.noreply.github.com>
2025-07-21 13:24:37 -0400

    Add redirects in Next.js configuration for team and self-started projects to streamline navigation to work pages.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const withNextra = require('nextra');
 const nextraConfig = {
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
+  async redirects() {
+    return [
+      {
+        source: '/team-projects/:slug*',
+        destination: '/work/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/self-started-projects/:slug*', 
+        destination: '/work/:slug*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withNextra(nextraConfig);
